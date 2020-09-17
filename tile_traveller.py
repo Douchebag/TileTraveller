70edf72f21f641ec8ba3f21262813d80823afcd1
Douchebag <yesimdingo@gmail.com>
2020-09-17 09:41:47 +0000

    algorithm updated

--- tile_traveller.py
+++ tile_traveller.py
@@ -1,5 +1,5 @@
 #  The program allows the user to input one of four directions, the user then travels to that direction
 #  depending on wether the direction is valid. Once the user has reached the end tile a message is displayed.
 #  The user starts on a tile [1, 1] and the victory tile is [3, 1]. 
-#  This should be able to be accomplished with ifelse loops inside a while loop.
+#  This should be able to be accomplished with ifelse loops inside a function and a while loop running in the main program.
 
